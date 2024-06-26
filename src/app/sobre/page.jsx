1aeaace10110d0ad77f367fcc8f83fe8330e68de
euukc <keniacarolineferreira@gmail.com>
2024-06-26 11:30:23 -0300

    style: ajuste responsividade mobile

--- src/app/sobre/page.jsx
+++ src/app/sobre/page.jsx
@@ -9,22 +9,22 @@ export default function About() {
             <div data-aos="fade-right" className="w-full md:w-[50%] flex justify-center items-center md:items-center flex-col relative md:left-[-350px] md:p-0 px-4 text-[20px] md:text-[35px] mt-[200px] md:mt-[300px] text-justify gap-y-10">
                 <h1>Sou a Byanka Dias, <span className="text-[#7042ccff]">especializada em modelagens personalizadas para Impressão 3D</span>. Busco constantemente inovação e tendências para garantir projetos únicos aos meus clientes.</h1>
                 <h2 className="text-center">Com atenção aos detalhes, transformo ideias em realidade.</h2>
-                <div className="flex gap-x-4">
-                    <div className="border-r-4 px-2 text-start">
+                <div className="flex gap-x-4 flex-wrap justify-center gap-y-2">
+                    <div className="border-r-2 md:border-r-4 px-2 text-start">
                         <span className="text-[#7042ccff] md:text-[36px]">1+</span>
-                        <p className="text-[12px]">ANOS DE EXPERIÊNCIA</p>
+                        <p className="text-[10px] md:text-[12px]">ANOS DE EXPERIÊNCIA</p>
                     </div>
-                    <div className="border-r-4 pr-2 text-start">
+                    <div className="border-r-2 md:border-r-4 pr-2 text-start">
                         <span className="text-[#7042ccff] md:text-[36px]">650+</span>
-                        <p className="text-[12px]">CLIENTES SATISFEITOS</p>
+                        <p className="text-[10px] md:text-[12px]">CLIENTES SATISFEITOS</p>
                     </div>
-                    <div className="border-r-4 pr-2 text-start">
+                    <div className="border-r-2 md:border-r-4 pr-2 text-start">
                         <span className="text-[#7042ccff] md:text-[36px]">100+</span>
-                        <p className="text-[12px]">PROJETOS FINALIZADOS</p>
+                        <p className="text-[10px] md:text-[12px]">PROJETOS FINALIZADOS</p>
                     </div>
                     <div>
                         <span className="text-[#7042ccff] md:text-[36px]">300+</span>
-                        <p className="text-[12px] text-start">PESSOAS DENTRO DA COMUNIDADE NO WHATSAPP</p>
+                        <p className="text-[10px] md:text-[12px] text-start">PESSOAS DENTRO DA COMUNIDADE NO WHATSAPP</p>
                     </div>
                     
                 </div>
